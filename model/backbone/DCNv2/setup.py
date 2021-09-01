106d19cf3c3654596bc4247448b9b42ad6e37887
Pyten <pan_teng@foxmail.com>
2021-09-01 13:00:08 +0800

    add original code

--- model/backbone/DCNv2/setup.py
+++ model/backbone/DCNv2/setup.py
@@ -14,57 +14,58 @@ from setuptools import setup
 
 requirements = ["torch", "torchvision"]
 
+
 def get_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "csrc")
+    extensions_dir = os.path.join(this_dir, "src")
 
     main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
     source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
     source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
-
+    
+    os.environ["CC"] = "g++"
     sources = main_file + source_cpu
     extension = CppExtension
-
     extra_compile_args = {"cxx": []}
     define_macros = []
 
+    
     if torch.cuda.is_available() and CUDA_HOME is not None:
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [("WITH_CUDA", None)]
         extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",  # Whether a short float (float16,fp16) is supported.
-            "-D__CUDA_NO_HALF_OPERATORS__", # https://github.com/pytorch/pytorch/blob/master/cmake/Dependencies.cmake#L1117
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
             "-D__CUDA_NO_HALF_CONVERSIONS__",
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
     else:
-        raise NotImplementedError("cuda is not available")
+        #raise NotImplementedError('Cuda is not available')
+        pass
+    
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
-
     include_dirs = [extensions_dir]
-
     ext_modules = [
         extension(
             "_ext",
-            # for converting onnx
-            # "deform_conv_cuda",
             sources,
             include_dirs=include_dirs,
             define_macros=define_macros,
             extra_compile_args=extra_compile_args,
         )
     ]
-
     return ext_modules
 
 setup(
-    name="monoflex",
-    version="0.0",
-    author="zyp",
+    name="DCNv2",
+    version="0.1",
+    author="charlesshang",
+    url="https://github.com/charlesshang/DCNv2",
+    description="deformable convolutional networks",
     packages=find_packages(exclude=("configs", "tests",)),
+    # install_requires=requirements,
     ext_modules=get_extensions(),
     cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
 )
-
